d3857f155397a8e18e41f86f7291f3bc5852f93d
David Schach <dschach@x2od.com>
2023-05-11 17:07:05 -0600

    chore: release v1.0.2

--- doc-assets/commitApexDox.sh
+++ doc-assets/commitApexDox.sh
@@ -3,10 +3,6 @@
 # Run this AFTER running ApexDox so all your files are set up properly.
 npm run prettier:docs
 
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-if [ $branch == "main" ]
-then
-		cd docs
-		git add .
-		git commit -m "docs(ApexDox): update ApexDox"
-fi
+cd docs
+git add .
+git commit -m "docs(ApexDox): update ApexDox"
